cf9311bd56c511f42e8ad419ed02f5e3fe9776fd
Harcic <harcic@outlook.com>
2024-07-01 15:59:27 +0800

    Added Quote.py & updated GroupManager.py

--- modules/GroupManager.py
+++ modules/GroupManager.py
@@ -3,14 +3,72 @@ from Hyper import Manager, ModuleClass, Segments, WordSafety
 
 class UserInfo:
     def __init__(self):
+        self.inited = False
         self.violation_level: int = 1
         self.violations = 0
         self.last_message: str = ""
         self.last_time: int = 0
         self.words_unsafe_times = 0
 
+    def inc_violations(self, num: int | float) -> None:
+        self.violations += num
 
-data: dict[int, UserInfo] = {}
+    def inc_unsafe_times(self) -> None:
+        self.words_unsafe_times += 1
+
+    def dec_unsafe_times(self) -> None:
+        self.words_unsafe_times -= 1
+
+    def clr_unsafe_times(self) -> None:
+        self.words_unsafe_times = 0
+        self.violation_level += 1
+
+    def dec_violations(self, num: int | float) -> None:
+        self.violations -= num
+
+    def clr_violations(self) -> None:
+        self.violations = 0
+        self.violation_level += 1
+
+    @property
+    def need_mute(self) -> bool:
+        return self.violations >= 12 or self.words_unsafe_times >= 3
+
+    def update(self, last_msg, last_time) -> None:
+        self.last_message = last_msg
+        self.last_time = last_time
+        self.inited = True
+
+
+class GroupInfo:
+    def __init__(self):
+        self.users: dict[int, UserInfo] = {}
+
+    def get_user(self, uin: int) -> UserInfo:
+        if uin in self.users:
+            return self.users[uin]
+        else:
+            self.users[uin] = UserInfo()
+            return self.users[uin]
+
+    def glb_dec(self) -> None:
+        for i in self.users:
+            self.users[i].dec_violations(1)
+
+
+class InfoManager:
+    def __init__(self):
+        self.groups: dict[int, GroupInfo] = {}
+
+    def get_group(self, gid: int) -> GroupInfo:
+        if gid in self.groups:
+            return self.groups[gid]
+        else:
+            self.groups[gid] = GroupInfo()
+            return self.groups[gid]
+
+
+data = InfoManager()
 
 
 def distance(s1: str, s2: str) -> int:
@@ -37,38 +95,51 @@ def string_similarity(s1: str, s2: str) -> float:
 @ModuleClass.ModuleRegister.register(["message"])
 class Module(ModuleClass.Module):
     async def handle(self):
-        if not data.get(self.event.user_id):
-            data[self.event.user_id] = UserInfo()
-            try:
-                data[self.event.user_id].last_message = str(self.event.message)
-            except AttributeError:
-                return None
-            data[self.event.user_id].last_time = self.event.time
-            return None
+        user = data.get_group(self.event.group_id).get_user(self.event.user_id)
+        if not user.inited:
+            user.update(str(self.event.message), self.event.time)
+            return
 
-        if string_similarity(data[self.event.user_id].last_message, str(self.event.message)) >= 0.66 \
-                or len(str(self.event.message)) >= 120 \
-                or self.event.time - data[self.event.user_id].last_time < 2:
-            if self.event.time - data[self.event.user_id].last_time < 2:
-                data[self.event.user_id].violations += 2
-            elif 2 < self.event.time - data[self.event.user_id].last_time < 20:
-                data[self.event.user_id].violations += 1
-            elif 20 < self.event.time - data[self.event.user_id].last_time < 60:
-                data[self.event.user_id].violations += 0.5
+        sim = string_similarity(user.last_message, str(self.event.message))
+
+        if self.event.time - user.last_time < 2:
+            user.inc_violations(2)
+        elif 2 < self.event.time - user.last_time < 20:
+            user.inc_violations(1)
+        elif 20 < self.event.time - user.last_time < 60:
+            user.inc_violations(0.5)
+        else:
+            pass
+
+        if sim < 0.66:
+            pass
+        elif 0.75 >= sim >= 0.66:
+            user.inc_violations(1)
+        elif 1 > sim >= 0.75:
+            user.inc_violations(1.5)
+        elif sim == 1:
+            if len(self.event.message) == 1 and isinstance(self.event.message[0], Segments.Image):
+                user.inc_violations(0.5)
             else:
-                data[self.event.user_id].violations += 0.1
+                user.inc_violations(2)
 
-        data[self.event.user_id].last_message = str(self.event.message)
-        data[self.event.user_id].last_time = self.event.time
+        if len(str(self.event.message)) < 50:
+            pass
+        elif 100 > len(str(self.event.message)) >= 50:
+            user.inc_violations(0.1)
+        elif 150 > len(str(self.event.message)) >= 100:
+            user.inc_violations(0.5)
+        elif 200 > len(str(self.event.message)) >= 150:
+            user.inc_violations(1)
+        else:
+            user.inc_violations(2)
 
-        for i in data:
-            data[i].violations -= 1 if i != self.event.user_id else 0
-            if data[i].violations < 0:
-                data[i].violations = 0
+        user.update(str(self.event.message), self.event.time)
+        data.get_group(self.event.group_id).glb_dec()
 
-        if data[self.event.user_id].violations >= 7:
+        if user.need_mute:
             await self.actions.set_group_ban(user_id=self.event.user_id, group_id=self.event.group_id,
-                                             duration=(60 * data[self.event.user_id].violation_level))
+                                             duration=(60 * user.violation_level))
             await self.actions.send(user_id=self.event.user_id, group_id=self.event.group_id,
                                     message=Manager.Message(
                                         [
@@ -77,8 +148,8 @@ class Module(ModuleClass.Module):
                                         ]
                                     )
                                     )
-            data[self.event.user_id].violations = 2
-            data[self.event.user_id].violation_level += 1
+            user.clr_violations()
+            user.inc_violations(2)
 
         safety = WordSafety.check(text=str(self.event.message))
         if not safety.result:
@@ -91,10 +162,10 @@ class Module(ModuleClass.Module):
                                         ]
                                     )
                                     )
-            data[self.event.user_id].words_unsafe_times += 1
-            if data[self.event.user_id].words_unsafe_times >= 3:
+            user.inc_unsafe_times()
+            if user.need_mute:
                 await self.actions.set_group_ban(user_id=self.event.user_id, group_id=self.event.group_id,
-                                                 duration=(60 * data[self.event.user_id].violation_level))
+                                                 duration=(60 * user.violation_level))
                 await self.actions.send(user_id=self.event.user_id, group_id=self.event.group_id,
                                         message=Manager.Message(
                                             [
@@ -103,5 +174,4 @@ class Module(ModuleClass.Module):
                                             ]
                                         )
                                         )
-                data[self.event.user_id].words_unsafe_times = 0
-                data[self.event.user_id].violation_level += 1
+                user.clr_unsafe_times()
---
+++ modules/Quote.py
@@ -0,0 +1,68 @@
+from Hyper import Segments
+from Hyper.Manager import gen_message, Message
+from Hyper.ModuleClass import Module, ModuleRegister
+from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
+import httpx
+import os
+
+
+def open_from_url(url: str):
+    return Image.open(BytesIO(httpx.get(url).content))
+
+
+def square_scale(image: Image, height: int):
+    old_width, old_height = image.size
+    x = height / old_height
+    width = int(old_width * x)
+    return image.resize((width, height))
+
+
+def wrap_text(text, chars_per_line=13):
+    lines = [text[i:i + chars_per_line] for i in range(0, len(text), chars_per_line)]
+    return '\n'.join(lines)
+
+
+def get_image(quote, ava_url, name):
+    mask = Image.open("assets/quote/mask.png").convert("RGBA")
+    background = Image.new('RGBA', mask.size, (255, 255, 255, 255))
+    head = open_from_url(ava_url).convert("RGBA")
+
+    title_font = ImageFont.truetype(r"assets\SourceHanSansSC-Bold-2.otf", size=36)
+    desc_font = ImageFont.truetype(r"assets\SourceHanSansSC-Regular-2.otf", size=20)
+    background.paste(square_scale(head, 640), (0, 0))
+    background.paste(mask, (0, 0), mask)
+    # background.show()
+    draw = ImageDraw.Draw(background)
+    text = wrap_text(quote)
+
+    draw.text((640, 165), text, font=title_font, fill=(255, 255, 255))
+    draw.text((862 if len(name) >= 7 else 1000, 465), f"——{name}", font=desc_font, fill=(112, 112, 112))
+    nbg = Image.new('RGB', mask.size, (0, 0, 0))
+    nbg.paste(background, (0, 0))
+    nbg.save("quote.png")
+
+
+@ModuleRegister.register(["message"])
+class Quoter(Module):
+    async def handle(self):
+        if ".quote" in str(self.event.message):
+            if isinstance(self.event.message[0], Segments.Reply):
+                msg_id = self.event.message[0].get()
+            else:
+                return
+
+            content = await self.actions.get_msg(msg_id)
+            name = content.data["sender"]["nickname"] if not content.data["sender"].get("card") else \
+                content.data["sender"]["card"]
+            uin = content.data["sender"]["user_id"]
+            message = content.data["message"]
+            message = gen_message({"message": message})
+            text = str(message)
+            get_image(text, f"http://q2.qlogo.cn/headimg_dl?dst_uin={uin}&spec=640", name)
+            await self.actions.send(group_id=self.event.group_id, user_id=self.event.user_id, message=Message(
+                [
+                    Segments.Reply(self.event.message_id),
+                    Segments.Image(f"file://{os.path.abspath('quote.png')}")
+                ]
+            ))
