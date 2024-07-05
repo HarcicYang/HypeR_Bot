06afeee9763ffc8ea84c67e3dfc1fd7f815e7293
Harcic <harcic@outlook.com>
2024-07-05 18:15:19 +0800

    Ess.py

---
+++ modules/Ess.py
@@ -0,0 +1,19 @@
+from Hyper.Manager import Message
+from Hyper.ModuleClass import Module, ModuleRegister
+from Hyper.Segments import *
+
+
+@ModuleRegister.register(["message"])
+class Ess(Module):
+    async def handle(self):
+        if isinstance(self.event.message.contents[0], Reply) and ".ess" in str(self.event.message):
+            msg_id = (await self.event.message.get())[0]["data"]["id"]
+            await self.actions.set_essence_msg(int(msg_id))
+            msg = Message(
+                [
+                    Reply(self.event.message_id),
+                    Text("成功")
+                ]
+            )
+            await self.actions.send(group_id=self.event.group_id, message=msg)
+
