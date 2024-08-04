d27721bc5a9340477343693da6e4139ec173277b
Harcic <harcic@outlook.com>
2024-08-04 13:28:32 +0800

    Add GayHub Workflow

---
+++ setup.py
@@ -0,0 +1,14 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+    name="hyper-bot",
+    version="0.7p",
+    description="稳定高效、易于开发的QQ Bot框架",
+    author="Harcic",
+    author_email="harcic@outlook.com",
+    url="https://github.com/HarcicYang/HypeR_Bot",
+    packages=["Hyper", "Hyper.Adapters", "Hyper.Adapters.OneBotLib"],
+    include_package_data=True,
+)
