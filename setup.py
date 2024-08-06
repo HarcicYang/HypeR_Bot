59be00a5620df36bc0fa07a597b304f98f3f4a3d
Harcic <harcic@outlook.com>
2024-08-06 16:01:36 +0800

    Updated version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="hyper-bot",
-    version="0.741",
+    version="0.75",
     description="稳定高效、易于开发的QQ Bot框架",
     author="Harcic",
     author_email="harcic@outlook.com",
