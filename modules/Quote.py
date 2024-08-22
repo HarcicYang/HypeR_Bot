a4d50da2d161588f556595dad0328adcfea0be77
Harcic <harcic@outlook.com>
2024-08-22 16:01:17 +0800

    New Quote (copied)

--- modules/Quote.py
+++ modules/Quote.py
@@ -3,46 +3,77 @@ from Hyper.Manager import Message
 from Hyper.Events import *
 from ModuleClass import Module, ModuleInfo, ModuleRegister
 
-from PIL import Image, ImageDraw, ImageFont
+# from PIL import Image, ImageDraw, ImageFont
+from pyppeteer import launch
 import os
-from io import BytesIO
-import httpx
 
 
-def open_from_url(url: str):
-    return Image.open(BytesIO(httpx.get(url).content))
+# from io import BytesIO
+# import httpx
 
 
-def square_scale(image: Image, height: int):
-    old_width, old_height = image.size
-    x = height / old_height
-    width = int(old_width * x)
-    return image.resize((width, height))
+# def open_from_url(url: str):
+#     return Image.open(BytesIO(httpx.get(url).content))
+#
+#
+# def square_scale(image: Image, height: int):
+#     old_width, old_height = image.size
+#     x = height / old_height
+#     width = int(old_width * x)
+#     return image.resize((width, height))
 
 
-def wrap_text(text, chars_per_line=13):
-    lines = [text[i:i + chars_per_line] for i in range(0, len(text), chars_per_line)]
-    return '\n'.join(lines)
+# def get_image(quote, ava_url, name):
+#     mask = Image.open("assets/quote/mask.png").convert("RGBA")
+#     background = Image.new('RGBA', mask.size, (255, 255, 255, 255))
+#     head = open_from_url(ava_url).convert("RGBA")
+#
+#     title_font = ImageFont.truetype(r"assets\SourceHanSansSC-Bold-2.otf", size=36)
+#     desc_font = ImageFont.truetype(r"assets\SourceHanSansSC-Regular-2.otf", size=20)
+#     background.paste(square_scale(head, 640), (0, 0))
+#     background.paste(mask, (0, 0), mask)
+#     # background.show()
+#     draw = ImageDraw.Draw(background)
+#     text = String(quote).format(20)
+#
+#     draw.text((640, 165), text, font=title_font, fill=(255, 255, 255))
+#     draw.text((862 if len(name) >= 7 else 1000, 465), f"——{name}", font=desc_font, fill=(112, 112, 112))
+#     nbg = Image.new('RGB', mask.size, (0, 0, 0))
+#     nbg.paste(background, (0, 0))
+#     nbg.save("./temps/quote.png")
+
+async def html2img(url: str) -> str:
+    browser = await launch(
+        headless=True,
+        options={
+            "handleSIGINT": False,
+            "handleSIGTERM": False,
+            "handleSIGHUP": False
+        }
+    )
+    page = await browser.newPage()
+    await page.goto(url)
+    title = await page.title()
+    await page.setViewport({"width": 1280, "height": 640})
+    path = f"./temps/web_{title.replace(' ', '')}.png"
+    await page.screenshot({"path": path})
+    await browser.close()
+    return path
 
 
-def get_image(quote, ava_url, name):
-    mask = Image.open("assets/quote/mask.png").convert("RGBA")
-    background = Image.new('RGBA', mask.size, (255, 255, 255, 255))
-    head = open_from_url(ava_url).convert("RGBA")
+async def get_image(quote, ava_url, name, uin):
+    with open("./assets/quote.html", "r", encoding="utf-8") as f:
+        html = f.read()
 
-    title_font = ImageFont.truetype(r"assets\SourceHanSansSC-Bold-2.otf", size=36)
-    desc_font = ImageFont.truetype(r"assets\SourceHanSansSC-Regular-2.otf", size=20)
-    background.paste(square_scale(head, 640), (0, 0))
-    background.paste(mask, (0, 0), mask)
-    # background.show()
-    draw = ImageDraw.Draw(background)
-    text = wrap_text(quote)
+    html = html.replace("{ava_url}", ava_url)
+    html = html.replace("{quote}", quote)
+    html = html.replace("{name}", name)
 
-    draw.text((640, 165), text, font=title_font, fill=(255, 255, 255))
-    draw.text((862 if len(name) >= 7 else 1000, 465), f"——{name}", font=desc_font, fill=(112, 112, 112))
-    nbg = Image.new('RGB', mask.size, (0, 0, 0))
-    nbg.paste(background, (0, 0))
-    nbg.save("./temps/quote.png")
+    with open(f"./temps/quote_{uin}.html", "w", encoding="utf-8") as f:
+        f.write(html)
+    res = await html2img(f"file://{os.path.abspath(f'./temps/quote_{uin}.html')}")
+    os.remove(f"./temps/quote_{uin}.html")
+    return res
 
 
 @ModuleRegister.register(GroupMessageEvent)
@@ -71,13 +102,13 @@ class Quoter(Module):
             message = content.data["message"]
             message = gen_message({"message": message})
             text = str(message)
-            get_image(text, f"http://q2.qlogo.cn/headimg_dl?dst_uin={uin}&spec=640", name)
+            res = await get_image(text, f"http://q2.qlogo.cn/headimg_dl?dst_uin={uin}&spec=640", name, uin)
             await self.actions.send(
                 group_id=self.event.group_id,
                 user_id=self.event.user_id,
                 message=Message(
                     Segments.Reply(self.event.message_id),
-                    Segments.Image(f"file://{os.path.abspath('./temps/quote.png')}")
+                    Segments.Image(f"file://{os.path.abspath(res)}")
                 )
             )
-            os.remove("./temps/quote.png")
+            os.remove(res)
